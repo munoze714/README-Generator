4ae410f331cdfd9a5fa51552c7c3040887604975
Elizabeth Munoz <munoz_714@live.com>
2020-04-11 17:08:44 -0700

    readme.md complete

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ inquirer
     {
         type: "input",
         name: "Usage",
-        message: "Provide instructions and examples for use. Include screenshots as needed.",
+        message: "Provide Usage instructions and examples for use. Include screenshots as needed.",
     },
     {
         type: "input",
@@ -40,48 +40,52 @@ inquirer
     {
         type: "input",
         name: "Tests",
-        message: "Write tests for your application. Then provide examples on how to run them."
-    },
-    {
-        type: "input",
-        name: "Questions",
-        message: "Any other Questions?"
+        message: "Are there tests for your application. Then provide examples on how to run them."
     },
     ])
     .then(function (answer) {
         // console.log(answer)
 
-        const queryUrl = `https://api.github.com/users/${answer.username}?acceses_token=0c8635d351a30c3534085cffdb89e924eb82b4b4`;
+        const queryUrl = `https://api.github.com/users/${answer.username}?access_token=ced8bce7e17bfc19c95a6b7a9799b7d0b1607edc`;
         axios.get(queryUrl).then((data) => {
             console.log('data from github!!!', data.data)
 
             var readme = `
-                #Project Title:
-                ${answer.Title}
-                ##Description:
-                ${answer.Description}
-                ##Table of Contents
-                *[Installation](#Installation)
-                *[Usage](#Usage)
-                *[License](#License)
-                *[Contributing](#Contributing)
-                *[Test](#Test)
-                ###Installation:
-                ${answer.Installation}
-                ###Usage:
-                ${answer.Usage}
-                ###Licence:
-                ${answer.License}
-                ###Contributing:
-                [![${answer.Contributing}](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md)
-                ###Test:
-                ${answer.Tests}
-                ###Questions:
-                ${answer.Questions}
-                ![alt text](${data.data.avatar_url})
-                ![alt text](${data.data.email})
+# Project Title:
+${answer.Title}
 
-                `
+## Description:
+${answer.Description}
+
+## Table of Contents
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributing](#Contributing)
+* [Test](#Test)
+
+### Installation:
+${answer.Installation}
+
+### Usage:
+${answer.Usage}
+
+### Licence:
+${answer.License}
+
+### Contributing:
+![${answer.Contributing}](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)
+
+### Test:
+${answer.Tests}
+
+### Questions:
+
+![alt text](${data.data.avatar_url})
+
+[${data.data.email}
+](${data.data.email})
+`
 
             fs.writeFile('readme.md', readme, (err) => {
                 if (err) throw err;
--- readme.md
+++ readme.md
@@ -1,27 +1,36 @@
 
-                #Project Title:
-                skflskj
-                ##Description:
-                fsdf
-                ##Table of Contents
-                *[Installation](#Installation)
-                *[Usage](#Usage)
-                *[License](#License)
-                *[Contributing](#Contributing)
-                *[Test](#Test)
-                ###Installation:
-                sfs
-                ###Usage:
-                sfs
-                ###Licence:
-                sf
-                ###Contributing:
-                [![sf](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md)
-                ###Test:
-                fs
-                ###Questions:
-                sf
-                ![alt text](https://avatars0.githubusercontent.com/u/59346164?v=4)
-                ![alt text](null)
+# Project Title:
+ReadMe Generator
 
-                
+## Description:
+As a Developer, I want a ReadMe Generator so that I can easily put together a good ReadMe for a New Project
+
+## Table of Contents
+
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributing](#Contributing)
+* [Test](#Test)
+
+### Installation:
+npm install Inquirer Axios
+
+### Usage:
+Follow prompt in Terminal
+
+### Licence:
+LizMunoz
+
+### Contributing:
+![Node.js](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)
+
+### Test:
+test are under constuction
+
+### Questions:
+
+![alt text](https://avatars0.githubusercontent.com/u/59346164?v=4)
+
+[munoze714@gmail.com
+](munoze714@gmail.com)
